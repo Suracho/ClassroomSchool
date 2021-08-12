0773a39ddb9a8fa6b86d6281d598b588e13e924e
suracho <suracho@outlook.com>
2021-08-12 19:14:20 +0530

    first

---
+++ src/main/java/com/example/schoolclassroom/SchoolClassroomApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolClassroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolClassroomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schoolclassroom/SchoolClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolclassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolClassroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
